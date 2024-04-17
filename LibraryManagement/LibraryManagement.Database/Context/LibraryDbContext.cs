12775ea69f95865735e4cebb95a6894e8e8d54a7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 15:30:45 +0300

    Create LibraryDbContext.cs

---
+++ LibraryManagement/LibraryManagement.Database/Context/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagement.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Database.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+    }
+}
