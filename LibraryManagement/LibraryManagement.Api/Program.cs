95abaf4e95c041cb899b8b5f3f4a8e97700fb847
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 17:32:26 +0300

    Call AddBusinessServices extension method in program.cs

--- LibraryManagement/LibraryManagement.Api/Program.cs
+++ LibraryManagement/LibraryManagement.Api/Program.cs
@@ -1,11 +1,14 @@
 using LibraryManagement.Database.Context;
 using Microsoft.EntityFrameworkCore;
+using LibraryManagement.Core.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddBusinessServices();
 
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
