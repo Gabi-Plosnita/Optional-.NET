f5a12941f7a18a9c425e44c7e164c9aa331cdad0
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 16:59:24 +0300

    Create extension method ToBookResponseDto for Book class

---
+++ LibraryManagement/LibraryManagement.Core/Mapping/BookMappingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagement.Core.Dtos.Response;
+using LibraryManagement.Database.Entities;
+
+namespace LibraryManagement.Core.Mapping
+{
+    public static class BookMappingExtensions
+    {
+        public static BookResponseDto ToBookResponseDto(this Book book)
+        {
+            var result = new BookResponseDto
+            {
+                Id = book.Id,
+                Title = book.Title,
+            };
+
+            return result;
+        }
+    }
+}
