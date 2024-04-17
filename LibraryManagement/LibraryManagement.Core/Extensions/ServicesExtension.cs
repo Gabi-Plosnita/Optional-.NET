017cb06ce748a13b852319122db855eccaec9035
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 17:27:45 +0300

    Create ServicesExtension and register Services and Repository interfaces

---
+++ LibraryManagement/LibraryManagement.Core/Extensions/ServicesExtension.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement.Core.Services;
+using LibraryManagement.Database.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryManagement.Core.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void AddBusinessServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+        }
+
+    }
+}
