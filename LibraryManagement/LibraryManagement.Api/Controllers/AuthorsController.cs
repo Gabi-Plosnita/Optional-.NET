3708632283ca245af571747314e7a2d1acea362c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 17:12:39 +0300

    Create AuthorsController.cs

---
+++ LibraryManagement/LibraryManagement.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Api.Controllers
+{
+    [Route("api/Authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        public AuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+    }
+}
