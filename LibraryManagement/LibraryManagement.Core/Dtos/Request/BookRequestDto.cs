94a1dad5fec7d3740500b1d0fcfed6a3901cbedd
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-17 16:43:45 +0300

    Create BookRequestDto.cs

---
+++ LibraryManagement/LibraryManagement.Core/Dtos/Request/BookRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Core.Dtos.Request
+{
+    public class BookRequestDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+    }
+}
