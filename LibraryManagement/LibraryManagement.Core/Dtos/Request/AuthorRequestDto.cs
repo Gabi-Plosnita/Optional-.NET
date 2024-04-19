63a3893b0067a0626b8b0a17e048023d8fbfb632
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-04-19 17:16:39 +0300

    Create AuthorRequestDto

---
+++ LibraryManagement/LibraryManagement.Core/Dtos/Request/AuthorRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Core.Dtos.Request
+{
+    public class AuthorRequestDto
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+    }
+}
