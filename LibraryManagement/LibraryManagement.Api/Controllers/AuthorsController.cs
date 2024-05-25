da456527421ea6e72558f141879ae1e0519c3cca
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-25 20:27:23 +0300

    Update AuthorsController.cs

--- LibraryManagement/LibraryManagement.Api/Controllers/AuthorsController.cs
+++ LibraryManagement/LibraryManagement.Api/Controllers/AuthorsController.cs
@@ -1,36 +1,38 @@
 ﻿using LibraryManagement.Core.Services;
 using Microsoft.AspNetCore.Mvc;
-using LibraryManagement.Core.Dtos.Response;
 using LibraryManagement.Core.Dtos.Request;
 using Microsoft.AspNetCore.Authorization;
 
 namespace LibraryManagement.Api.Controllers
 {
+    [Authorize(Roles = "Author")]
     [Route("api/Authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly IAuthorService _authorService;
-        public AuthorsController(IAuthorService authorService)
+        private readonly IBookService _bookService;
+        public AuthorsController(IAuthorService authorService, IBookService bookService)
         {
             _authorService = authorService;
+            _bookService = bookService;
         }
 
-        [Authorize(Roles= "Admin")]
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-
-        public ActionResult<List<AuthorResponseDto>> GetAuthors()
-        {
-            return Ok(_authorService.GetAuthors());
-        }
-
-        [Authorize(Roles = "Author")]
-        [HttpPost]
+        [HttpPost("/CreateAuthor")]
         [ProducesResponseType(StatusCodes.Status201Created)]
 
         public ActionResult<int> CreateAuthor([FromBody] AuthorRequestDto author)
         {
             int id = _authorService.CreateAuthor(author);
+            return StatusCode(StatusCodes.Status201Created, id);
+        }
+
+        [HttpPost("CreateBook")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<int> CreateBook([FromBody] BookRequestDto book)
+        {
+            int id = _bookService.CreateBook(book);
             return StatusCode(StatusCodes.Status201Created, id);
         }
     }
