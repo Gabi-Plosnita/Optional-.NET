734fdfbf06279d90e353e60bad6016fed6c95021
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-25 17:58:22 +0300

    Create and implement IUserRepository

---
+++ LibraryManagement/LibraryManagement.Database/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagement.Database.Entities;
+
+namespace LibraryManagement.Database.Repositories
+{
+    public interface IUserRepository
+    {
+        void Register(User user);
+
+        User GetByEmail(string email);
+
+        List<User> GetAll();
+
+        User Get(int id);
+
+        void Update(int id, User updatedUser);
+
+        void Delete(int id);
+    }
+}
