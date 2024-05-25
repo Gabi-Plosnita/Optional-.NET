84859d137a8c91c8a55009b17a3946924c87cdb9
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-25 17:33:28 +0300

    Create and implement IRoleRepository

---
+++ LibraryManagement/LibraryManagement.Database/Repositories/RoleRepository/IRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagement.Database.Entities;
+
+namespace LibraryManagement.Database.Repositories
+{
+    public interface IRoleRepository
+    {
+        List<Role> GetAll();
+
+        Role Get(int id);
+
+        void Create(Role role);
+
+        void Update(int id, Role updatedRole);
+
+        void Delete(int id);
+    }
+}
---
+++ LibraryManagement/LibraryManagement.Database/Repositories/RoleRepository/RoleRepository.cs
@@ -0,0 +1,65 @@
+﻿using LibraryManagement.Database.Context;
+using LibraryManagement.Database.Entities;
+
+namespace LibraryManagement.Database.Repositories.RoleRepository
+{
+    public class RoleRepository : BaseRepository, IRoleRepository
+    {
+        public RoleRepository(LibraryDbContext context) : base(context)
+        {
+        }
+        public List<Role> GetAll()
+        {
+            return _libraryDbContext.Roles.ToList();
+        }
+
+        public Role Get(int id)
+        {
+            var role = _libraryDbContext.Roles.Find(id);
+            if (role == null)
+            {
+                throw new Exception("Role not found");
+            }
+            return role;
+        }
+
+        public void Create(Role role)
+        {
+            if(_libraryDbContext.Roles.Any(r => r.Name == role.Name))
+            {
+                throw new Exception($"Role with name {role.Name} already exists");
+            }
+            _libraryDbContext.Roles.Add(role);
+            SaveChanges();
+        }
+
+        public void Update(int id, Role updatedRole)
+        {
+            var role = _libraryDbContext.Roles.Find(id);
+            if (role == null)
+            {
+                throw new Exception("Role not found");
+            }
+
+            if(_libraryDbContext.Roles.Any(r => r.Name == updatedRole.Name))
+            {
+                throw new Exception($"Role with name {updatedRole.Name} already exists");
+            }
+
+            role.Name = updatedRole.Name;
+            SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var role = _libraryDbContext.Roles.Find(id);
+            if (role == null)
+            {
+                throw new Exception("Role not found");
+            }
+
+            _libraryDbContext.Roles.Remove(role);
+            SaveChanges();
+        }
+    }
+}
