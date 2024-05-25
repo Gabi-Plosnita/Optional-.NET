7f8d6477184bc20a1153a3e49cbc606a263f125c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-25 17:47:31 +0300

    Add navigation property to role and Update Context class

--- LibraryManagement/LibraryManagement.Database/Context/LibraryDbContext.cs
+++ LibraryManagement/LibraryManagement.Database/Context/LibraryDbContext.cs
@@ -14,5 +14,18 @@ namespace LibraryManagement.Database.Context
         public DbSet<Role> Roles { get; set; }
 
         public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(a => a.Books)
+                .WithOne(b => b.Author)
+                .HasForeignKey(b => b.AuthorId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+        }
     }
 }
