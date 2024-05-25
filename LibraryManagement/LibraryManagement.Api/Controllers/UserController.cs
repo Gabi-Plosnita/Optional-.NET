77777327c3c7ea8bf471fbc04067452a22fbd618
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-05-25 19:29:52 +0300

    Create UserController.cs

---
+++ LibraryManagement/LibraryManagement.Api/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using LibraryManagement.Core.Dtos.Request;
+using LibraryManagement.Core.Dtos.Response;
+using LibraryManagement.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Api.Controllers
+{
+    [Route("api/Users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("/register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult Register([FromBody] UserRequestDto user)
+        {
+            try
+            {
+                _userService.Register(user);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("/login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<string> Login([FromBody] LoginRequestDto login)
+        {
+            try
+            {
+                string token = _userService.Login(login);
+                return Ok(token);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<List<UserResponseDto>> GetAll()
+        {
+            return Ok(_userService.GetAll());
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult Update([FromRoute] int id, [FromBody] UserRequestDto user)
+        {
+            try
+            {
+                _userService.Update(id, user);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                _userService.Delete(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
